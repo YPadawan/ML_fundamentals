'Modifying line search and conjugate gradient, something is wrong needs fixing!'

algorithms/logistic_regression.py
@@ -11,9 +11,30 @@ def logistic(x):
 
     Returns
     -------
+    x_tr: float,
+        transformation logistique de x
+    """
+    return 1.0 / (1.0 + np.exp(-x))
+
+def stable_logistic(x):
+    """Applique la régression logistique en fonction du signe de x afin de le rendre plus stable
+
+    Parameters
+    ----------
+    x: float,
+        Valeur auxquelles on applique la transformation de x
+
+    Returns
+    -------
+    x_tr: float,
+        Transformation logistique de x
 
     """
-    return (1.0 / (1.0 + np.exp(-x)))
+    return np.where(
+        x <= 0.,
+        np.exp(x) / (1.0 + np.exp(-x)),
+        logistic(x)
+    )
 
 
 def logistic_surrogate_loss(X, y, w):
@@ -33,8 +54,9 @@ def logistic_surrogate_loss(X, y, w):
     n, d = X.shape
     S = 0.
     ps = 0.
-    ps += np.dot(X,w[:d]).sum()
-    S += (logistic(y*ps)).sum() / n
+    ps += np.dot(X, w[:d]).sum()
+    S += stable_logistic(y*ps).sum() / n
+    # S += logistic(y*ps).sum() / n
     return S
 
 
@@ -61,8 +83,8 @@ def gradient_logistic_surrogate_loss(X, y, w):
     ps += np.dot(X, w[:d]).sum()
     g[-1] = 0.
     for i in range(n):
-        g[-1] += ((logistic(y * ps) - 1.0) * y).sum()
-        g[:d] += np.dot((logistic(y * ps) - 1.0) * y, X)
+        g[-1] += ((stable_logistic(y * ps) - 1.0) * y).sum()
+        g[:d] += np.dot((stable_logistic(y * ps) - 1.0) * y, X)
 
     g /= n
     return g

algorithms/optimizers.py
@@ -1,36 +1,42 @@
 import numpy as np
 
 
-# TODO:  Harmonize var names
-def line_search(X, y, w, cost_func, p, g, old_loss, new_loss, w_old):
+def line_search(X, y, cost_func, p, g, old_loss, w_old):
     """Line search algorithm
+
     Parameters
     ----------
+    X : array_like, shape (n_samples, n_features)
+        Matrix containing data
+    y : array, shape (n_samples,)
+        True labels
+    w : array, shape (n_features + 1,)
+        Weight vector
+    cost_func : callable
+        Cost function
+    p : array, shape (n_features + 1,)
+        descent direction
+    g : array, shape (n_features + 1,)
+        gradient vector
+    old_loss : float
+        old loss value
+    new_loss : float
+        new loss value
+    w_old : array, shape (n_features,)
+        old weight vector
 
-    X : array_like, shape (n, d),
-        Matrice contenant les données
-    y : array, shape (n,)
-        Vecteur des labels
-    w : array, (d+1,)
-        Vecteur des poids avec le biais en plus
-    cost_func : callable,
-        Fonction de coût
-    p : array, (d+1,)
-        Vecteur de la direction de descente
-    g : array (d+1,)
-        Vecteur gradient
+    Returns
+    -------
+    w : array, shape (n_features + 1,)
+        Updated weight vector
     """
+    import pdb
+    pdb.set_trace()
 
     alpha = 1e-4
-    mineta = 1e-7
+    min_eta = 1e-7
 
     n, d = X.shape
-    # initialisation de w_old
-    # w_old = w.copy()
-
-    # Initialisation de old_loss et new_loss
-    # old_loss = cost_func(X, y, w)
-    # new_loss = old_loss
 
     # Calcul de la pente au point actuel (float)
     pente = p @ g
@@ -40,7 +46,7 @@ def line_search(X, y, w, cost_func, p, g, old_loss, new_loss, w_old):
     for j in range(d+1):
         if np.abs(p[j]) > _max * np.maximum(np.abs(w_old[j]), 1.):
             _max = np.abs(p[j]) / np.maximum(np.abs(w_old[j]), 1.)
-    etamin = mineta / _max
+    eta_min = min_eta / _max
 
     # Initialisation de eta à 1
     eta = 1.
@@ -52,13 +58,13 @@ def line_search(X, y, w, cost_func, p, g, old_loss, new_loss, w_old):
 
     # Boucler tant que la condition d'Armijo n'est pas complète (Eq. 2.18)
     while new_loss > (old_loss + alpha * eta * pente):
-        if eta < etamin:
+        if eta < eta_min:
             w = w_old
             break
         else:
             if eta == 1.:
                 # Minimiseur du polynôme d'interpolation de degré 2 (Eq. 2.32)
-                etatmp = -pente / (2 * (new_loss - old_loss - pente))
+                eta_tmp = -pente / (2 * (new_loss - old_loss - pente))
             else:
                 coeff1 = new_loss - old_loss - eta * pente
                 coeff2 = new_loss2 - old_loss - eta2 * pente
@@ -67,30 +73,55 @@ def line_search(X, y, w, cost_func, p, g, old_loss, new_loss, w_old):
                 c = np.array([coeff1, coeff2]).reshape(2, 1)
 
                 # Calcul des coefficients du polynôme d'interpolation de degré 3 (Eq. 2.33)
-                ab = (1 / (eta - eta2) * np.dot(d, c)).reshape(1, -1)
-                a, b = ab[0][0], ab[0][1]
+                a, b = 1 / (eta - eta2) * np.dot(d, c).flatten()
 
                 if a != 0.:
                     delta = (b ** 2) - 3. * a * pente
                     if delta >= 0.:
                         # minimiseur du polynôme d'interpolation de degré 3 (Eq 2.34)
-                        etatmp = (-b + np.sqrt(delta)) / (3. * a)
+                        eta_tmp = (-b + np.sqrt(delta)) / (3. * a)
                     else:
                         raise ValueError("rchln:problème d'interpolation")
 
                 else:
-                    etatmp = -pente / (2.*b)
-                    if etatmp > 0.5*eta:
-                        etatmp = 0.5*eta
+                    eta_tmp = -pente / (2.*b)
+                if eta_tmp > 0.5*eta:
+                    eta_tmp = 0.5*eta
         eta2 = eta
         new_loss2 = new_loss
-        eta = np.maximum(etatmp, 0.1 * eta)
+        eta = np.maximum(eta_tmp, 0.1 * eta)
         w = w_old + eta * p
         new_loss = cost_func(X, y, w)
+        print(w)
     return w, new_loss
 
 
 def conjugate_gradient(X, y, w, cost_func, grd_func, epsilon):
+    """Computes the conjugate gradient
+
+    Parameters
+    ----------
+    X : array_like, shape (n_samples, n_features)
+        Matrix containing data
+    y : array, shape (n_samples,)
+        True labels
+    w : array, shape (n_features,)
+        Weight vector
+    cost_func : callable
+        Cost function
+    grd_func : callable
+        Gradient function
+    epsilon : float
+
+
+    Returns
+    -------
+    Nothing, updates the weight vector with the loss minimizing value
+
+    """
+    import pdb
+    pdb.set_trace()
+
     epoque = 0
     n, d = X.shape
     w_old = np.random.random(d+1)
@@ -101,13 +132,14 @@ def conjugate_gradient(X, y, w, cost_func, grd_func, epsilon):
     g = grd_func(X, y, w)
     # Initialisation de p au gradient en temps 0 (Eq. 2.46)
     p = -g
-    # print(p)
+    print(new_loss)
 
     while np.abs(old_loss - new_loss) > np.abs(old_loss) * epsilon:
         old_loss = new_loss
         w, new_loss = line_search(X=X, y=y, w=w, cost_func=cost_func,
                                   p=p, g=g, old_loss=old_loss,
                                   new_loss=new_loss, w_old=w_old)
+        print(new_loss)
         #         print(w)
         # Calcul du nouveau vecteur gradient (Eq. 2.42)
         h = grd_func(X, y, w)
@@ -125,4 +157,6 @@ def conjugate_gradient(X, y, w, cost_func, grd_func, epsilon):
         p = -g + beta * p
 
         print(f"Epoque {epoque} and loss is: {new_loss}")
+        print(w)
         epoque += 1
+    return w